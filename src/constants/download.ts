10876aa1c9b766ba13bf53ebc49dc8542d0ca1c0
andforce <wdyrix@gmail.com>
2026-09-03 06:39:04 -0700

    init

--- src/constants/download.ts
+++
@@ -1,4 +0,0 @@
-export const OCTRIX_MACOS_DMGS = {
-  appleSilicon: 'https://github.com/andforce/octrix/releases/latest/download/Octrix-arm64.dmg',
-  intel: 'https://github.com/andforce/octrix/releases/latest/download/Octrix-x64.dmg',
-} as const
